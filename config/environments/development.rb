7312eb8704777be701e33bf73a9331096fe61fe3
Andres Montano <amontano@virginia.edu>
2019-03-22 11:02:20 -0600

    updating terms_engine to 0.4.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,10 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
+  
+  # When doing recordings importation using forks the asyncjob gets stuck if not runnign inline
+  config.active_job.queue_adapter = :inline
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
