93f6225ffb9bcb8d0a8120cd1504a104d65431cd
Andres Montano <amontano@virginia.edu>
2020-05-17 21:05:09 -0600

    updated authenticated_system to 2.3.9, complex_dates to 2.2.9, flare to 1.2.4, kmaps_engine to 5.8.6, and subjects_integration to 0.2.7.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
   
   # When doing recordings importation using forks the asyncjob gets stuck if not runnign inline
-  config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = :delayed_job
   
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
